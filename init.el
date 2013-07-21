97627a486164341b235fd6288eff3ccee7426cf1
Greg Hawkins <github@greghawkins.net>
2013-07-21 12:13:55 +0100

    Put distraction template on F12.

--- init.el
+++ init.el
@@ -148,9 +148,15 @@
 (setq org-refile-allow-creating-parent-nodes (quote confirm))
 
 (global-set-key "\C-cl" 'org-store-link)
-(global-set-key "\C-cc" 'org-capture)
 (global-set-key "\C-ca" 'org-agenda)
 (global-set-key "\C-cb" 'org-iswitchb)
+
+(global-set-key "\C-cc" 'org-capture)
+(global-set-key [f12] 'org-capture)
+
+;; Distraction on f12
+(define-key global-map [f12]
+  (lambda () (interactive) (org-capture nil "d")))
 
 ;;=============================================================================
 ;; Enable disabled features
