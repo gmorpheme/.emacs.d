361b8cc95123272c62ebf4434a3ec47f89ecf0b9
Greg Hawkins <github@greghawkins.net>
2013-11-07 22:30:02 +0000

    Updated bizet.el.

---
+++ bizet.el
@@ -0,0 +1,4 @@
+;; fix git location
+(setq magit-git-executable "C:/Program Files (x86)/git/bin/git.exe")
+(setq cider-lein-command "e:/local/dev/lein/lein.bat")
+
--- sibelius.el
+++ sibelius.el
@@ -1,4 +1,4 @@
 ;; fix git location
 (setq magit-git-executable "C:/Program Files/git/bin/git.exe")
-(setq nrepl-lein-command "c:/local/dev/lein/lein2.bat")
+(setq nrepl-lein-command "c:/local/dev/lein/lein.bat")
 
