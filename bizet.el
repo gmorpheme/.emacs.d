50be23f6349d65928afdcceee1c2a728da0b7fae
Greg Hawkins <github@greghawkins.net>
2014-03-07 09:37:39 +0000

    Octave location for bizet.

--- bizet.el
+++ bizet.el
@@ -1,4 +1,5 @@
 ;; fix git location
 (setq magit-git-executable "C:/Program Files (x86)/git/bin/git.exe")
 (setq cider-lein-command "e:/local/dev/lein/lein.bat")
+(setq inferior-octave-program "e:/Octave-3.6.4/bin/octave.exe")
 
