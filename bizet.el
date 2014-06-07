61e3b81a914b9bcd64c38d274f469249fae0c1b1
Greg Hawkins <github@greghawkins.net>
2014-06-07 18:12:23 +0100

    Set magit dirs for bizet.

--- bizet.el
+++ bizet.el
@@ -2,4 +2,5 @@
 (setq magit-git-executable "C:/Program Files (x86)/git/bin/git.exe")
 (setq cider-lein-command "e:/local/dev/lein/lein.bat")
 (setq inferior-octave-program "e:/Octave-3.6.4/bin/octave.exe")
+(setq magit-repo-dirs '("e:/local/dev" "e:/local/.emacs.d"))
 
